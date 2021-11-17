9e73e93d94900d53c46b7629e89ea675ab492450
sniezhokrm <sniezhokrm@gmail.com>
2021-11-17 07:02:46 +0200

    mask tel and text valid

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from './modules/modals';
 import slides from './modules/slides';
+import forms from './modules/forms';
+import checkTextInputs from './modules/checkNumInputs';
+import mask from './modules/mask';
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -8,4 +11,8 @@ window.addEventListener('DOMContentLoaded', () => {
   modals();
   slides('.feedback-slider-item', '.main-next-btn', '.main-prev-btn');
   slides('.main-slider-item', '', '', 'vertical');
+  forms();
+  mask('[name = "phone"]');
+  checkTextInputs('[name = "name"]');
+  checkTextInputs('[name = "message"]');
 })
