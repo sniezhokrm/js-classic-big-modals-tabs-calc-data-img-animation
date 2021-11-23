4c0fcea54c3186fa8930b2b1e3d95468877f1fe6
sniezhokrm <sniezhokrm@gmail.com>
2021-11-23 11:55:03 +0200

    add burger menu

--- dist/script.js
+++ dist/script.js
@@ -42,6 +42,37 @@ const accordion = triggersSelector => {
 
 /***/ }),
 
+/***/ "./src/js/modules/burger.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/burger.js ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const burger = (btnBurgerSelector, menuBurgerSelector) => {
+  let btnBurger = document.querySelector(btnBurgerSelector),
+      menuBurger = document.querySelector(menuBurgerSelector);
+  menuBurger.style.display = 'none';
+  btnBurger.addEventListener('click', () => {
+    if (menuBurger.style.display == 'none' && window.screen.availWidth < 993) {
+      menuBurger.style.display = 'block';
+    } else {
+      menuBurger.style.display = 'none';
+    }
+  });
+  window.addEventListener('resize', () => {
+    if (window.screen.availWidth > 992) {
+      menuBurger.style.display = 'none';
+    }
+  });
+  s;
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (burger);
+
+/***/ }),
+
 /***/ "./src/js/modules/calc.js":
 /*!********************************!*\
   !*** ./src/js/modules/calc.js ***!
@@ -2382,6 +2413,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_filter__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./modules/filter */ "./src/js/modules/filter.js");
 /* harmony import */ var _modules_pictureSize__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./modules/pictureSize */ "./src/js/modules/pictureSize.js");
 /* harmony import */ var _modules_accordion__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./modules/accordion */ "./src/js/modules/accordion.js");
+/* harmony import */ var _modules_burger__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./modules/burger */ "./src/js/modules/burger.js");
+
 
 
 
@@ -2407,6 +2440,7 @@ window.addEventListener('DOMContentLoaded', () => {
   (0,_modules_filter__WEBPACK_IMPORTED_MODULE_7__["default"])();
   (0,_modules_pictureSize__WEBPACK_IMPORTED_MODULE_8__["default"])('.sizes-block');
   (0,_modules_accordion__WEBPACK_IMPORTED_MODULE_9__["default"])('.accordion-heading');
+  (0,_modules_burger__WEBPACK_IMPORTED_MODULE_10__["default"])('.burger', '.burger-menu');
 });
 }();
 /******/ })()
---
+++ src/js/modules/burger.js
@@ -0,0 +1,24 @@
+const burger = (btnBurgerSelector, menuBurgerSelector) => {
+  let btnBurger = document.querySelector(btnBurgerSelector),
+    menuBurger = document.querySelector(menuBurgerSelector);
+
+  menuBurger.style.display = 'none';
+
+  btnBurger.addEventListener('click', () => {
+    if (menuBurger.style.display == 'none' && window.screen.availWidth < 993) {
+      menuBurger.style.display = 'block';
+    } else {
+      menuBurger.style.display = 'none';
+    }
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.screen.availWidth > 992) {
+      menuBurger.style.display = 'none';
+    }
+  });
+
+  s
+};
+
+export default burger;
